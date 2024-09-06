99770811808f373eb5c792f515861bcc299e6794
martbul <martbul01@gmail.com>
2024-09-06 21:52:44 +0300

    add cors

--- server/go.mod
+++ server/go.mod
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-openapi/analysis v0.23.0 // indirect
 	github.com/go-openapi/errors v0.22.0 // indirect
@@ -25,6 +26,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/martbul/microservices/data"
 	"github.com/martbul/microservices/handlers"
@@ -50,11 +51,14 @@ func main() {
 	//serving a the swagger.yaml file
 	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	//CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
+
 	// create new server
 	server := &http.Server{
 		Addr: ":9090",
 		// using the new created serverMux, instead of the default
-		Handler:      serveMux,
+		Handler:      ch(serveMux),
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
